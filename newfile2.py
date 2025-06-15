159f235dcc976b058c8f192e144c98e8ba6b17d5
𝕽𝖆𝖍𝖎𝖒🥶 <sh31khrxh1m3@gmail.com>
2025-06-15 12:29:58 +0100

    Update newfile2.py

--- newfile2.py
+++ newfile2.py
@@ -1,8 +1,9 @@
 import cv2,os
+import numpy as np
 
-diamond = cv2.imread("Assets/diamond addition.jpeg")
-star = cv2.imread("Assets/star addition.jpeg")
-bobby = cv2.imread("Assets/bobby.png")
+# diamond = cv2.imread("Assets/diamond addition.jpeg")
+# star = cv2.imread("Assets/star addition.jpeg")
+# bobby = cv2.imread("Assets/bobby.png")
 
 #add = cv2.addWeighted(star,0.9,diamond,1,0)
 #cv2.imshow("bob+",add)
@@ -20,8 +21,8 @@ bobby = cv2.imread("Assets/bobby.png")
 # Median Blur - used in digital processing preserves edges but removes noise 
 # Bilateral Blur - only sharp edges are preserved here
 
-bilateral = cv2.imread("Assets/bilateral.jpg")
-saltandpeppergrains = cv2.imread("Assets/salt and pepper grains.jpeg")
+# bilateral = cv2.imread("Assets/bilateral.jpg")
+# saltandpeppergrains = cv2.imread("Assets/salt and pepper grains.jpeg")
 
 # gaussianblur = cv2.GaussianBlur(bilateral,(101,101),0,0)
 # cv2.imshow("bob+++",gaussianblur)
@@ -31,6 +32,19 @@ saltandpeppergrains = cv2.imread("Assets/salt and pepper grains.jpeg")
 # cv2.imshow("bob++++",medianblur)
 # cv2.waitKey(0)
 
-bilateralfilter = cv2.bilateralFilter(bilateral,7,101,101)
-cv2.imshow("bob+++++",bilateralfilter)
+# bilateralfilter = cv2.bilateralFilter(bilateral,7,101,101)
+# cv2.imshow("bob+++++",bilateralfilter)
+# cv2.waitKey(0)
+
+# medianblur = cv2.medianBlur(median,7,9,10)
+# cv2.imshow("bob+++++",medianblur)
+# cv2.waitKey(0)
+
+pika = cv2.imread("Assets/pika.png")
+# border = cv2.copyMakeBorder(people,50,50,50,50,cv2.BORDER_REPLICATE)
+# cv2.imshow("bob++++++",border)
+# cv2.waitKey(0)
+kernel = np.ones((100,100),np.uint8)
+erosion = cv2.erode(pika,kernel)
+cv2.imshow("bob+++++++",erosion)
 cv2.waitKey(0)
